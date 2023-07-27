6f2cc9d2085b708faefeccba7a1d83a759417598
hervekongo96 <hervekongo.hk@gmail.com>
2023-07-27 11:55:03 +0100

    Create functionality

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const messageOne = ['Mbote !', 'Bonjour !', 'Biche !', 'Jambo !'];
 const messageTwo = ['To lingi yo te', "Tu n'es pas la bienvenue", 'Toka Apa', 'Kiangekebeni'];
 
 
+//using the fonctinality
+const afficheOne = messageOne[Math.floor(Math.random() * messageOne.length)];
+const afficheTwo = messageTwo[Math.floor(Math.random() * messageTwo.length)];
+
+
+// global message
+const message = `${afficheOne}, ${afficheTwo}`;
+
+
+// affichage
+console.log(message);
