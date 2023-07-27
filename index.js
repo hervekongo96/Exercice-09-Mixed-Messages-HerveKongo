e68395a660b1e347c342d9f0e646881c0216c529
hervekongo96 <hervekongo.hk@gmail.com>
2023-07-27 12:31:20 +0100

    modif name commentaire

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// declaration the tab
+// declaration the tableau
 
 const messageOne = ['Mbote !', 'Bonjour !', 'Biche !', 'Jambo !'];
 const messageTwo = ['To lingi yo te', "Tu n'es pas la bienvenue", 'Toka Apa', 'Kiangekebeni'];
