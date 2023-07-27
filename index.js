239cb62fc51441ffb196370face9cb3b928957d9
hervekongo96 <hervekongo.hk@gmail.com>
2023-07-27 11:41:46 +0100

    declaration tableau

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// initialisation
+// declaration the tab
+
+const messageOne = ['Mbote !', 'Bonjour !', 'Biche !', 'Jambo !'];
+const messageTwo = ['To lingi yo te', "Tu n'es pas la bienvenue", 'Toka Apa', 'Kiangekebeni'];
+
+
